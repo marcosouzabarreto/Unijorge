1d70a82457d2898b9d6109e3683f3f1825335ce8
Marco Barreto <marcosouzabarreto@gmail.com>
2022-03-29 18:18:56 -0300

    created basic gui

---
+++ Compiladores/Compilador/src/compiler.py
@@ -0,0 +1,3 @@
+while True:
+    text = input("baianes >_")
+    print(text)
---
+++ Compiladores/Compilador/src/lexer.py
@@ -0,0 +1,6 @@
+## Criar objeto para tokens
+
+class Token:
+    def __init__(self, token_type, value):
+        self.type = token_type
+        self.value = value
